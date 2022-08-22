d3f336c0818ce87a8e93dbb0f9c4e7b2fe133035
Henry Chung <chungthehao@gmail.com>
2022-08-22 10:24:05 +0700

    Setup Express Server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const keys = require('./keys')
+
+
+// Express App Setup
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
