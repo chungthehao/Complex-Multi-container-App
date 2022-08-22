01205fa586149e57903a3712c5c22cf83ebc82ec
Henry Chung <chungthehao@gmail.com>
2022-08-22 10:25:26 +0700

    Postgres Client setup for the Express server

--- server/index.js
+++ server/index.js
@@ -9,3 +9,20 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+
+// Postgres Client Setup
+const { Pool } = require('pg')
+const pgClient = new Pool({
+  host: keys.pgHost,
+  port: keys.pgPort,
+  database: keys.pgDatabase,
+  user: keys.pgUser,
+  password: keys.pgPassword
+})
+
+pgClient.on('connect', client => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch(err => console.error(err))
+})
