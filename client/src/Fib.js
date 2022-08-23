cb438db268bdd7b3013e473cead3ad7bae7ebe51
Henry Chung <chungthehao@gmail.com>
2022-08-23 09:57:52 +0700

    Fib page logic

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Fib() {
-  const [seenIndexes, setSeenIndexes] = useState([])
-  const [values, setValues] = useState({})
+  const [seenIndexes, setSeenIndexes] = useState(null)
+  const [values, setValues] = useState(null)
   const [index, setIndex] = ''
 
   const fetchValues = async () => {
@@ -16,6 +16,12 @@ function Fib() {
     setSeenIndexes(pgSeenIndexes.data)
   }
 
+  const handleSubmit = async e => {
+    e.preventDefault()
+    await axios.post('/api/values', { index })
+    setIndex('')
+  }
+
   useEffect(() => {
     fetchValues()
     fetchIndexes()
@@ -23,7 +29,27 @@ function Fib() {
 
   return (
     <div>
+      <form onChange={handleSubmit}>
+        <label>
+          <span>Enter your index: </span>
+          <input
+            type="number" 
+            value={index}
+            onChange={e => setIndex(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
 
+      <h3>Indexes I have seen: </h3>
+      {seenIndexes && seenIndexes.map(({ number }) => number).join(', ') + '.'}
+
+      <h3>Calculated values: </h3>
+      {values && values.forEach((value, ind) => (
+        <div key={ind}>
+          For index {ind} - I calculated {value}
+        </div>
+      ))}
     </div>
   );
 }
